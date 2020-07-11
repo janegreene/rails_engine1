e11ea069194b214bfaaedcdc22b148b7ef64ff68
Jane Greene <janegreenecasa@gmail.com>
2020-07-11 17:08:29 -0600

    created database tables

--- db/migrate/20200711205955_create_invoices.rb
+++ db/migrate/20200711205955_create_invoices.rb
@@ -1,8 +1,8 @@
 class CreateInvoices < ActiveRecord::Migration[5.1]
   def change
     create_table :invoices do |t|
-      t.references :customer_id, foreign_key: true
-      t.references :merchant_id, foreign_key: true
+      t.references :customer, foreign_key: true
+      t.references :merchant, foreign_key: true
       t.string :status
 
       t.timestamps
--- db/migrate/20200711210710_create_transactions.rb
+++ db/migrate/20200711210710_create_transactions.rb
@@ -1,7 +1,7 @@
 class CreateTransactions < ActiveRecord::Migration[5.1]
   def change
     create_table :transactions do |t|
-      t.references :invoice_id, foreign_key: true
+      t.references :invoice, foreign_key: true
       t.string :credit_card_number
       t.date :credit_card_expiration_date
       t.string :result
