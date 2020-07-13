a600272be55260a4970ca917bf5235df596871fb
Jane Greene <janegreenecasa@gmail.com>
2020-07-13 16:26:19 -0600

    passing tests for items index show and create but not working on local host for serializer

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,21 +1,23 @@
 class Api::V1::ItemsController < ApplicationController
 
   def index
-    render json: Item.all
+    # items = Item.all
+    # require "pry"; binding.pry
+    render json: ItemSerializer.new(Item.all)
   end
 
   def show
-    render json: Item.find(params[:id])
+    render json: ItemSerializer.new(Item.find(params[:id]))
   end
 
   def create
-    render json: Item.create(item_params)
+    render json: ItemSerializer.new(Item.create(item_params))
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:name, :description, :unit_price)
+    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
   end
 
 end
--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -5,10 +5,12 @@ describe "Items API" do
     create_list(:item, 3)
 
     get '/api/v1/items'
-
     expect(response).to be_successful
+
     items = JSON.parse(response.body)
-    expect(items.count).to eq(3)
+    expect(items['data'].count).to eq(3)
+    expect(items).to be_instance_of(Hash)
+    expect(items['data']).to be_instance_of(Array)
   end
   it "can get one item by its id" do
    id = create(:item).id
@@ -18,15 +20,20 @@ describe "Items API" do
    item = JSON.parse(response.body)
 
    expect(response).to be_successful
-   expect(item["id"]).to eq(id)
+   expect(item).to be_instance_of(Hash)
+   expect(item['data']["id"]).to eq(id.to_s)
  end
- # it "can create a new item" do
- #    item_params = { name: "Banana", description: "Yellow tree part.", unit_price: 4.00 }
- #
- #    post "/api/v1/items", params: {item: item_params}
- #    item = Item.last
- #
- #    expect(response).to be_successful
- #    expect(item.name).to eq(item_params[:name])
- # end
+ it "can create a new item" do
+    merchant = create(:merchant)
+    item_params = { name: "Banana", description: "Yellow tree part.", unit_price: 4.00, merchant_id: "#{merchant.id}" }
+
+    post "/api/v1/items", params: {item: item_params}
+    item = Item.last
+
+    expect(response).to be_successful
+    expect(item.name).to eq(item_params[:name])
+    expect(item.description).to eq("Yellow tree part.")
+    expect(item.unit_price).to eq(4.00)
+    expect(item.merchant_id).to eq(merchant.id)
+ end
 end
