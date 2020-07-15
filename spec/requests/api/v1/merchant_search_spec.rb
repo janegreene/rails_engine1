2602b13526e71556c94ffa1d6c67f62b594bf801
Jane Greene <janegreenecasa@gmail.com>
2020-07-14 19:11:14 -0600

    merchant find all by segment passing spec harness

---
+++ spec/requests/api/v1/merchant_search_spec.rb
@@ -0,0 +1,25 @@
+describe "merchant search endpoints" do
+    it 'can find a list of merchants that contain a fragment, case insensitive' do
+      merchant_1 = create(:merchant, name: "Tillman")
+      merchant_2 = create(:merchant, name: "Hilly Hillman")
+      merchant_3 = create(:merchant, name: "Hello")
+      get '/api/v1/merchants/find_all?name=ILL'
+
+      json = JSON.parse(response.body, symbolize_names: true)
+      names = json[:data].map do |merchant|
+        merchant[:attributes][:name]
+      end
+      expect(response).to be_successful
+
+      expect(names.sort).to eq(["Hilly Hillman", "Tillman"])
+    end
+
+    # it 'can find a merchants that contain a fragment, case insensitive' do
+    #   response = conn('/api/v1/merchants/find?name=ILL').get
+    #   json = JSON.parse(response.body, symbolize_names: true)
+    #   name = json[:data][:attributes][:name].downcase
+    #
+    #   expect(json[:data]).to be_a(Hash)
+    #   expect(name).to include('ill')
+    # end
+end
