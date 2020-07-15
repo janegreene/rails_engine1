a3b6aed4c1ffa53cdc7d7316f61e4fe878e366a9
Jane Greene <janegreenecasa@gmail.com>
2020-07-15 12:03:16 -0600

    updated model relationships for revenue BI feature still WIP

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,6 +2,8 @@ class Invoice < ApplicationRecord
   belongs_to :customer
   belongs_to :merchant
   has_many :invoice_items
+  has_many :transactions
+  # belongs_to :transaction
   has_many :items, through: :invoice_items
   validates :customer_id, presence: true
   validates :merchant_id, presence: true
--- spec/requests/api/v1/revenue_request_spec.rb
+++ spec/requests/api/v1/revenue_request_spec.rb
@@ -1,5 +1,28 @@
 describe 'business intelligence' do
   it 'can get merchants with most revenue' do
+    customer_1 = create(:customer)
+    invoice_1 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_1.id)
+    invoice_2 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_2.id)
+    invoice_3 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_3.id)
+    merchant_1 = create(:merchant)
+    merchant_2 = create(:merchant)
+    merchant_3 = create(:merchant)
+    item_1 = create(:item, merchant_id: merchant_1.id)
+    item_2 = create(:item, merchant_id: merchant_1.id)
+    item_3 = create(:item, merchant_id: merchant_2.id)
+    item_4 = create(:item, merchant_id: merchant_2.id)
+    item_5 = create(:item, merchant_id: merchant_2.id)
+    item_6 = create(:item, merchant_id: merchant_3.id)
+    item_7 = create(:item, merchant_id: merchant_3.id)
+    invoice_item_1 = create(:invoice_item, invoice_id: invoice_1.id, item_id: item_1.id)
+    invoice_item_2 = create(:invoice_item, invoice_id: invoice_1.id, item_id: item_2.id)
+    invoice_item_3 = create(:invoice_item, invoice_id: invoice_1.id, item_id: item_3.id)
+    invoice_item_4 = create(:invoice_item, invoice_id: invoice_2.id, item_id: item_4.id)
+    invoice_item_5 = create(:invoice_item, invoice_id: invoice_2.id, item_id: item_5.id)
+    invoice_item_6 = create(:invoice_item, invoice_id: invoice_2.id, item_id: item_6.id)
+    invoice_item_7 = create(:invoice_item, invoice_id: invoice_3.id, item_id: item_7.id)
+    invoice_item_8 = create(:invoice_item, invoice_id: invoice_3.id, item_id: item_1.id)
+    invoice_item_9 = create(:invoice_item, invoice_id: invoice_3.id, item_id: item_2.id)
     # get "/api/v1/merchants/most_revenue?quantity=7"
     # json = JSON.parse(response.body, symbolize_names: true)
     #
@@ -13,5 +36,10 @@ describe 'business intelligence' do
     #
     # expect(json[:data][6][:attributes][:name]).to eq("Rath, Gleason and Spencer")
     # expect(json[:data][6][:id]).to eq("53")
+
+    
+#     data = Merchant.joins(invoices:[:invoice_items, :transactions]).where("tr
+# ansactions.result='success'").group("merchants.name").select("merchants.name, sum(invoice_
+# items.quantity*invoice_items.unit_price) as revenue").order("revenue desc").limit(5)
   end
 end
