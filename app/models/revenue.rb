2aafcddb1e1c53875d3f21053cb7e8a7c59c27e1
Jane Greene <janegreenecasa@gmail.com>
2020-07-15 18:40:51 -0600

    revenue serializer needs work

--- app/models/revenue.rb
+++ app/models/revenue.rb
@@ -1,10 +1,12 @@
 class Revenue
   def initialize(revenue)
-    @id = null
+    @id = nil
     @revenue = revenue
   end
 
-  def total_revenue_for_timespan(params)
-    require "pry"; binding.pry
+  def self.total_revenue_for_timespan(params)
+    revenue = Invoice.joins(:invoice_items, :transactions).merge(Transaction.where(result: 'success')).where('invoices.created_at BETWEEN ? AND ?', params[:start], params[:end]).sum('quantity * unit_price')
+    # test = InvoiceItem.select(sum(invoice_items.quantity*invoice_items.unit_price) as revenue").joins(:invoice).where('invoices.created_at BETWEEN ? AND ?', params[:start], params[:end]).where("transactions.result ='success'").sum(invoice_items.quantity*invoice_items.unit_price)
+    new revenue
   end
 end
